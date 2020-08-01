105deade8480f5466f50e097c512eb6457399ea5
wyuefzhuol <1527169088@qq.com>
2020-08-01 17:18:08 +0800

    [Eric] test: should_return_employee_response_when_add_employees_given_employee

---
+++ src/main/java/com/thoughtworks/springbootemployee/util/ConstantInterface.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.springbootemployee.util;
+
+public interface ConstantInterface {
+    String EMPLOYEE_REQUEST_JSON_PAY = "{\n" +
+            "    \"name\": \"Eric\",\n" +
+            "    \"gender\": \"male\",\n" +
+            "    \"age\": \"18\",\n" +
+            "    \"companyId\": \"1\"\n" +
+            "}";
+}
---
+++ src/test/java/com/thoughtworks/springbootemployee/IntegrationTest/EmployeeIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.thoughtworks.springbootemployee.IntegrationTest;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import com.thoughtworks.springbootemployee.util.ConstantInterface;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Rollback
+public class EmployeeIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Test
+    void should_return_employee_response_when_add_employees_given_employee() throws Exception {
+        Company company = new Company("oocl");
+        companyRepository.save(company);
+        mockMvc.perform(post("/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ConstantInterface.EMPLOYEE_REQUEST_JSON_PAY))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("Eric"));
+    }
+}
